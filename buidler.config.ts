6df9358d2af96d44d7ca9c603e3c63205eee71f6
Mick de Graaf <mick@mickdegraaf.nl>
2020-10-19 14:02:34 +0200

    Buidler dependencies

---
+++ buidler.config.ts
@@ -0,0 +1,32 @@
+import { usePlugin } from  "@nomiclabs/buidler/config";
+
+usePlugin("@nomiclabs/buidler-ethers");
+usePlugin('solidity-coverage')
+
+const config = {
+  defaultNetwork: 'buidlerevm',
+  networks: {
+    buidlerevm: {
+      gasPrice: 0,
+      blockGasLimit: 100000000,
+    },
+    localhost: {
+      url: 'http://localhost:8545'
+    },
+    coverage: {
+      url: 'http://localhost:8555'
+    },
+    frame: {
+      url: "http://localhost:1248"
+    }
+  },
+  solc: {
+    version: '0.7.1',
+    optimizer: {
+      enabled: false,
+      runs: 200
+    }
+  },
+}
+
+export default config;

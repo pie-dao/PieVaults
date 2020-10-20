5aa64c851ddab23b4032ca119e2dcf50912b466d
Mick de Graaf <mick@mickdegraaf.nl>
2020-10-21 00:18:34 +0200

    Linting

--- contracts/interfaces/IExperiPie.sol
+++ contracts/interfaces/IExperiPie.sol
@@ -6,18 +6,28 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface IExperiPie is IERC20 {
     // function initialize(address[] memory _tokens, uint256 _maxCap) external;
     function joinPool(uint256 _amount) external;
-    function exitPool(uint256 _amount) external;
-    function getLock() external view returns(bool);
-    function getLockBlock() external view returns(uint256);
-    function setLock(uint256 _lock) external;
-    function getMaxCap() external view returns(uint256);
-    function setMaxCap(uint256 _maxCap) external returns(uint256);
-    function balance(address _token) external view returns(uint256);
-    function getTokens() external view returns (address[] memory);
-    function addToken(address _token) external;
-    function removeToken(address _token) external;
-    function getTokenInPool(address _token) external view returns(bool);
 
+    function exitPool(uint256 _amount) external;
+
+    function getLock() external view returns (bool);
+
+    function getLockBlock() external view returns (uint256);
+
+    function setLock(uint256 _lock) external;
+
+    function getMaxCap() external view returns (uint256);
+
+    function setMaxCap(uint256 _maxCap) external returns (uint256);
+
+    function balance(address _token) external view returns (uint256);
+
+    function getTokens() external view returns (address[] memory);
+
+    function addToken(address _token) external;
+
+    function removeToken(address _token) external;
+
+    function getTokenInPool(address _token) external view returns (bool);
 
     // ERC20 facet
 
--- contracts/test/DiamondFactoryContract.sol
+++ contracts/test/DiamondFactoryContract.sol
@@ -5,24 +5,24 @@ pragma experimental ABIEncoderV2;
 import "diamond-3/contracts/Diamond.sol";
 
 contract DiamondFactoryContract {
-  event DiamondCreated(address tokenAddress);
+    event DiamondCreated(address tokenAddress);
 
-  address[] public diamonds;
-  mapping(address => bool) public isDiamond;
+    address[] public diamonds;
+    mapping(address => bool) public isDiamond;
 
-  function deployNewDiamond(
-    address _owner,
-    IDiamondCut.FacetCut[] memory _diamondCut
-  ) public returns (address) {
-    Diamond d = new Diamond(_diamondCut, _owner);
+    function deployNewDiamond(
+        address _owner,
+        IDiamondCut.FacetCut[] memory _diamondCut
+    ) public returns (address) {
+        Diamond d = new Diamond(_diamondCut, _owner);
 
-    diamonds.push(address(d));
-    isDiamond[address(d)] = true;
+        diamonds.push(address(d));
+        isDiamond[address(d)] = true;
 
-    emit DiamondCreated(address(d));
-  }
+        emit DiamondCreated(address(d));
+    }
 
-  function getDiamondCount() external view returns (uint256) {
-    return diamonds.length;
-  }
+    function getDiamondCount() external view returns (uint256) {
+        return diamonds.length;
+    }
 }

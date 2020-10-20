cc76e4b4e7b7018c35b8cdb9c06f9bf7169b9eb6
Mick de Graaf <mick@mickdegraaf.nl>
2020-10-20 12:01:25 +0200

    Seperate config for compilation

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,7 +1,14 @@
-import { usePlugin } from  "@nomiclabs/buidler/config";
+import { usePlugin, task } from  "@nomiclabs/buidler/config";
+
+import {Signer, Wallet, utils, constants} from "ethers";
+import {deployContract} from "ethereum-waffle";
+
+import DiamondFactoryArtifact from './artifacts/DiamondFactory.json';
+import DiamondFactory from "./typechain/factories/DiamondFactory";
 
 usePlugin("@nomiclabs/buidler-ethers");
-usePlugin('solidity-coverage')
+usePlugin('solidity-coverage');
+
 
 const config = {
   defaultNetwork: 'buidlerevm',
@@ -28,5 +35,13 @@ const config = {
     }
   },
 }
+
+task("deploy-diamond-factory")
+  .setAction(async(taskArgs, {ethers}) => {
+    const signers = await ethers.getSigners();
+
+    const diamondFactory = (await deployContract(signers[0] as Wallet, DiamondFactoryArtifact, [])) as DiamondFactory;
+
+});
 
 export default config;

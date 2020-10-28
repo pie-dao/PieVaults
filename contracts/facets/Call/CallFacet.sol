a4eac9b929d1f9d384897c512d890e02b1a2c544
Mick de Graaf <mick@mickdegraaf.nl>
2020-10-29 00:01:54 +0100

    Add whitelisted callers

--- contracts/facets/Call/CallFacet.sol
+++ contracts/facets/Call/CallFacet.sol
@@ -5,8 +5,55 @@ pragma solidity ^0.7.1;
 import "diamond-2/contracts/libraries/LibDiamond.sol";
 import "../shared/Reentry/ReentryProtection.sol";
 import "../shared/Access/CallProtection.sol";
+import "./LibCallStorage.sol";
 
-contract CallFacet is ReentryProtection, CallProtection {
+contract CallFacet is ReentryProtection {
+
+  event CallerAdded(address indexed caller);
+  event CallerRemoved(address indexed caller);
+
+  // uses modified call protection modifier to also allow whitelisted addresses to call
+  modifier protectedCall() {
+    require(
+        msg.sender == LibDiamond.diamondStorage().contractOwner ||
+        LibCallStorage.callStorage().canCall[msg.sender] ||
+        msg.sender == address(this), "NOT_ALLOWED"
+    );
+    _;
+  }
+
+  function addCaller(address _caller) external {
+    require(msg.sender == LibDiamond.diamondStorage().contractOwner, "NOT_ALLOWED");
+    LibCallStorage.CallStorage storage callStorage = LibCallStorage.callStorage();
+
+    require(!callStorage.canCall[msg.sender], "IS_ALREADY_CALLER");
+
+    callStorage.callers.push(_caller);
+    callStorage.canCall[_caller] = true;
+
+    emit CallerAdded(_caller);
+  }
+
+  function removeCaller(address _caller) external {
+    require(msg.sender == LibDiamond.diamondStorage().contractOwner, "NOT_ALLOWED");
+    LibCallStorage.CallStorage storage callStorage = LibCallStorage.callStorage();
+
+    require(callStorage.canCall[msg.sender], "IS_NOT_CALLER");
+
+    for(uint256 i = 0; i < callStorage.callers.length; i ++) {
+      address currentCaller = callStorage.callers[i];
+
+      // if found remove it
+      if(currentCaller == _caller) {
+        callStorage.callers[i] = callStorage.callers[callStorage.callers.length - 1];
+        callStorage.callers.pop();
+        break;
+      }
+    }
+
+    emit CallerRemoved(_caller);
+  }
+
   function call(
     address[] memory _targets,
     bytes[] memory _calldata,

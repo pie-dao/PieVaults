5d8a204c9338a026882d32201be598674d15b6ff
Mick de Graaf <mick@mickdegraaf.nl>
2020-11-10 16:13:17 +0100

    LendingRegistry NatSpec

--- contracts/callManagers/LendingManager/LendingRegistry.sol
+++ contracts/callManagers/LendingManager/LendingRegistry.sol
@@ -23,27 +23,56 @@ contract LendingRegistry is Ownable {
     event WrappedToUnderlyingSet(address indexed wrapped, address indexed underlying);
     event ProtocolToLogicSet(bytes32 indexed protocol, address indexed logic);
     event UnderlyingToProtocolWrappedSet(address indexed underlying, bytes32 indexed protocol, address indexed wrapped);
-    
+
+    /**
+        @notice Set which protocl a wrapped token belongs to
+        @param _wrapped Address of the wrapped token
+        @param _protocol Bytes32 key of the protocol
+    */
     function setWrappedToProtocol(address _wrapped, bytes32 _protocol) onlyOwner external {
         wrappedToProtocol[_wrapped] = _protocol;
         emit WrappedToProtocolSet(_wrapped, _protocol);
     }
 
+    /**
+        @notice Set what is the underlying for a wrapped token
+        @param _wrapped Address of the wrapped token
+        @param _underlying Address of the underlying token
+    */
     function setWrappedToUnderlying(address _wrapped, address _underlying) onlyOwner external {
         wrappedToUnderlying[_wrapped] = _underlying;
         emit WrappedToUnderlyingSet(_wrapped, _underlying);
     }
 
+    /**
+        @notice Set the logic contract for the protocol
+        @param _protocol Bytes32 key of the procol
+        @param _logic Address of the lending logic contract for that protocol
+    */
     function setProtocolToLogic(bytes32 _protocol, address _logic) onlyOwner external {
         protocolToLogic[_protocol] = _logic;
         emit ProtocolToLogicSet(_protocol, _logic);
     }
 
+    /**
+        @notice Set the wrapped token for the underlying deposited in this protocol
+        @param _underlying Address of the unerlying token
+        @param _protocol Bytes32 key of the protocol
+        @param _wrapped Address of the wrapped token
+    */
     function setUnderlyingToProtocolWrapped(address _underlying, bytes32 _protocol, address _wrapped) onlyOwner external {
         underlyingToProtocolWrapped[_underlying][_protocol] = _wrapped;
         emit UnderlyingToProtocolWrappedSet(_underlying, _protocol, _wrapped);
     } 
 
+    /**
+        @notice Get tx data to lend the underlying amount in a specific protocol
+        @param _underlying Address of the underlying token
+        @param _amount Amount to lend
+        @param _protocol Bytes32 key of the protocol
+        @return targets Addresses of the contracts to call
+        @return data Calldata for the calls
+    */
     function getLendTXData(address _underlying, uint256 _amount, bytes32 _protocol) external view returns(address[] memory targets, bytes[] memory data) {
         ILendingLogic lendingLogic = ILendingLogic(protocolToLogic[_protocol]);
         require(address(lendingLogic) != address(0), "NO_LENDING_LOGIC_SET");
@@ -51,6 +80,13 @@ contract LendingRegistry is Ownable {
         return lendingLogic.lend(_underlying, _amount);
     }
 
+    /**
+        @notice Get the tx data to unlend the wrapped amount
+        @param _wrapped Address of the wrapped token
+        @param _amount Amount of wrapped token to unlend
+        @return targets Addresses of the contracts to call
+        @return data Calldata for the calls
+    */
     function getUnlendTXData(address _wrapped, uint256 _amount) external view returns(address[] memory targets, bytes[] memory data) {
         ILendingLogic lendingLogic = ILendingLogic(protocolToLogic[wrappedToProtocol[_wrapped]]);
         require(address(lendingLogic) != address(0), "NO_LENDING_LOGIC_SET");

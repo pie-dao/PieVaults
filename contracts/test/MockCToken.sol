09976ec9e8d0e658ebfab02c9616d290dbe37ce3
Mick de Graaf <mick@mickdegraaf.nl>
2020-12-23 20:04:24 +0100

    WIP

--- contracts/test/MockCToken.sol
+++ contracts/test/MockCToken.sol
@@ -3,20 +3,22 @@ pragma experimental ABIEncoderV2;
 pragma solidity ^0.7.1;
 
 import "./MockToken.sol";
+import "../interfaces/ICToken.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
-contract MockCToken is MockToken {
+contract MockCToken is MockToken, ICToken {
     using SafeMath for uint256;
     uint256 public exchangeRate = 1 ether / 5;
     MockToken public underlying;
+    uint256 someValue;
 
     uint256 public errorCode;
     constructor(address _underlying) MockToken("cTOKEN", "cToken") public {
         underlying = MockToken(_underlying);
     }
 
-    function mint(uint256 _amount) external returns(uint256) {
+    function mint(uint256 _amount) external override returns(uint256) {
         require(underlying.transferFrom(msg.sender, address(this), _amount), "MockCToken.mint: transferFrom failed");
 
         uint256 mintAmount = _amount.mul(10**18).div(exchangeRate);
@@ -25,11 +27,7 @@ contract MockCToken is MockToken {
         return errorCode;
     }
 
-    function exchangeRateStored() external view returns(uint256) {
-        return exchangeRate;
-    }
-
-    function redeem(uint256 _amount) external returns(uint256) {
+    function redeem(uint256 _amount) external override returns(uint256) {
         _burn(msg.sender, _amount);
 
         uint256 underlyingAmount = _amount.mul(exchangeRate).div(10**18);
@@ -53,5 +51,18 @@ contract MockCToken is MockToken {
 
     function setErrorCode(uint256 _value) public {
         errorCode = _value;
+    }
+
+    
+    function exchangeRateCurrent() external override returns(uint256) {
+        // To make function state changing
+        someValue ++;
+        return exchangeRate;
+    }
+
+    function exchangeRateStored() external override view returns(uint256) {
+        // To make function non pure;
+        someValue;
+        return exchangeRate;
     }
 }

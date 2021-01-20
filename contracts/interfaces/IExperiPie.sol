02845d24d3d8a154ae527225b16f1395c9fef2ed
Mick de Graaf <mick@mickdegraaf.nl>
2021-01-20 17:37:21 +0100

    Include DiamondCut and DiamondLoupe in IExperiPie interface

--- contracts/interfaces/IExperiPie.sol
+++ contracts/interfaces/IExperiPie.sol
@@ -4,6 +4,8 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@pie-dao/diamond/contracts/interfaces/IERC173.sol";
+import "@pie-dao/diamond/contracts/interfaces/IDiamondLoupe.sol";
+import "@pie-dao/diamond/contracts/interfaces/IDiamondCut.sol";
 import "./IBasketFacet.sol";
 import "./IERC20Facet.sol";
 import "./ICallFacet.sol";
@@ -12,5 +14,5 @@ import "./ICallFacet.sol";
     @title ExperiPie Interface
     @dev Combines all ExperiPie facet interfaces into one
 */
-interface IExperiPie is IERC20, IBasketFacet, IERC20Facet, IERC173, ICallFacet {
+interface IExperiPie is IERC20, IBasketFacet, IERC20Facet, IERC173, ICallFacet, IDiamondLoupe, IDiamondCut {
 }
